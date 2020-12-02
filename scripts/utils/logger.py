5b06e21ad778a1e189f5532b6c85257592b00cba
vuongt <vuonganhtuyet102@gmail.com>
2020-12-02 22:01:37 +0100

    Set utf8 for logger

--- scripts/utils/logger.py
+++ scripts/utils/logger.py
@@ -2,6 +2,8 @@ import logging
 import os
 
 LOG_DIR = "logs"
+
+
 def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
@@ -17,7 +19,7 @@ def get_logger(name):
 
     if not (os.path.exists(LOG_DIR)):
         os.mkdir(LOG_DIR)
-    file_handler = logging.FileHandler(os.path.join(LOG_DIR, "donghanh.log"))
+    file_handler = logging.FileHandler(os.path.join(LOG_DIR, "donghanh.log"), encoding='utf-8')
     file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.DEBUG)
     logger.addHandler(file_handler)
